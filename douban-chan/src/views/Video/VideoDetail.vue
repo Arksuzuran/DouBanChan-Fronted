af0589e27c06b6f096adc53c2b178d058418ef55
David-A <77474977+darrenglow@users.noreply.github.com>
2023-05-27 22:10:38 +0800

    影视详情页,影评详情页,富文本编辑器

--- douban-chan/src/views/Video/VideoDetail.vue
+++ douban-chan/src/views/Video/VideoDetail.vue
@@ -302,7 +302,7 @@ export default {
 </script>
 
 <style scoped>
- /*.right-side{
+.right-side{
   left: 1000px;
   top: 100px;
   position: fixed;
@@ -322,25 +322,6 @@ export default {
 }
  .el-row {
     margin-bottom: 20px;
-    &:last-child {
-      margin-bottom: 0;
-    }
-  }
-  .el-col {
-    border-radius: 4px;
-  }
-  .bg-purple-dark {
-    background: #99a9bf;
-  }
-  .bg-purple {
-    background: #d3dce6;
-  }
-  .bg-purple-light {
-    background: #e5e9f2;
-  }
-  .grid-content {
-    border-radius: 4px;
-    min-height: 36px;
   }
   .row-bg {
     padding: 10px 0;
